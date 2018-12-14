1fc031b9d395f3c87aa6518f55078b14922c74fd
qupan <34915305+qupan@users.noreply.github.com>
2018-12-14 16:17:16 +0800

    Update variable.py

--- data/variable.py
+++ data/variable.py
@@ -1,1 +1,29 @@
 
+# -*- coding:utf-8 -*-
+
+url = "http://demo"
+auth=('111','111')
+headers = {'content-type': "text/xml","Accept":'application/xml'}
+
+
+# 01-得到获取用户信息
+GET_CUSTOMERINFO_list=[
+('1','1','1'),('1','1','1'),('1','1','1'),('1','1','1'),
+('1','1','1'),('1','20','1'),('','20','1'),
+('1','','1'),('1','20','1'),('1','20','1'),('1','20','1'),]
+GET_CUSTOMERINFO_DATA='''<soapenv:Envelope xmlns:soapenv="http://demo">
+   <soapenv:Header/>
+   <soapenv:Body>
+      <urn:MT_GET_CUSTOMERINFO_REQ>
+         <!--Optional:-->
+         <VKORG>%s</VKORG>
+         <!--Optional:-->
+         <VTWEG>%s</VTWEG>
+         <!--Zero or more repetitions:-->
+         <ITEM>
+            <!--Optional:-->
+            <KUNNR>%s</KUNNR>
+         </ITEM>
+      </urn:MT_GET_CUSTOMERINFO_REQ>
+   </soapenv:Body>
+</soapenv:Envelope>'''
